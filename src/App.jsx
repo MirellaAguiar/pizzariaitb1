47cfc9e2c294f1c9d2080b6a0523778ec236e729
MirellaAguiar <mirellapiresaguiar@gmail.com>
2024-08-06 11:46:46 -0300

    trabalgo final pizzaria

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import {useState} from 'react'
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Listagem from './pages/Listagem';
- 
+
 const App = () => {
- 
+
   const [color] = useState("#0000FF")
   document.body.style.backgroundColor = color
- 
+
   return (
     <Routes>
-      <Route path="/" element={<Listagem />} />
+      <Route path="/" element={<Listagem />}/>
     </Routes>
   )
 }
+
 export default App;
+
---
+++ src/pages/Pizzas.jsx
@@ -0,0 +1,40 @@
+import mussarela from "../assets/imagens/mussarela.png"
+import calabresa from "../assets/imagens/calabresa.png"
+import atum from "../assets/imagens/atum.png"
+import portuguesa from "../assets/imagens/portuguesa.png"
+import frango from "../assets/imagens/frango.png"
+
+const Pizzas = [
+    {
+      "id": 1,
+      "nome": "Mussarela",
+      "descricao": "Molho de tomate e mussarela.",
+      "imagem": mussarela
+    },
+    {
+      "id": 2,
+      "nome": "Frango com Catupiry",
+      "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.",
+      "imagem": frango
+    },
+    {
+      "id": 3,
+      "nome": "Portuguesa",
+      "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
+      "imagem": portuguesa
+    },
+    {
+      "id": 4,
+      "nome": "Calabresa",
+      "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
+      "imagem": calabresa
+    },
+    {
+      "id": 5,
+      "nome": "Atum",
+      "descricao": "Molho de tomate, mussarela, atum e tomate.",
+      "imagem": atum
+    },
+  ]
+
+  export default Pizzas;
